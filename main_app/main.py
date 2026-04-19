8c8db89b686d8c7e274800d10fcd4f5286a1fc3e
AliAbdou92019 <aliabdougithub@gmail.com>
2026-04-19 19:31:08 +0200

    Include the products_router in the main.py

--- main_app/main.py
+++ main_app/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
-from database import engine  
+from database import engine  , Base
 from models import *
+from routers.products import router as products_router
 
 app = FastAPI(title="E-Commerce app")
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+app.include_router(products_router)
 
 @app.get("/")
 def root():
