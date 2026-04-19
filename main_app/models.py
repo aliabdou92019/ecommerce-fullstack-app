c656acaf185c0b84973e10e4dd0f520cb5775838
AliAbdou92019 <aliabdougithub@gmail.com>
2026-04-19 22:52:00 +0200

    Add (role) column to User for Role-based access

--- main_app/models.py
+++ main_app/models.py
@@ -10,6 +10,7 @@ class User(Base):
     username = Column(String(50), unique=True, index=True, nullable=False)
     email = Column(String(100), unique=True, index=True, nullable=False)
     password = Column(String(255), nullable=False)
+    role = Column(String(20), default="user", nullable=False)
     created_at = Column(DateTime, default=datetime.astimezone)
     orders = relationship("Order", back_populates="owner")
 
