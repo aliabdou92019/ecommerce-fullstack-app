5f53ac2e8b543142129ff494d78a858d7010a80f
amiraazzam25 <amiraazzam937@gmail.com>
2026-04-21 16:49:35 +0200

    added the router to the main

--- main_app/main.py
+++ main_app/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 from database import engine  
 from models import *
-
+from routers.users import router as users_router
 app = FastAPI(title="E-Commerce app")
 Base.metadata.create_all(bind=engine)
-
+app.include_router(users_router)
 app = FastAPI()
 
 @app.get("/")
