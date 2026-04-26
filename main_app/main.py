b09822d871957afc2676a46854e5e6fb5a1855ab
maria <mariagerges28@gmail.com>
2026-04-27 00:59:22 +0300

    adding redis connection to the main

--- main_app/main.py
+++ main_app/main.py
@@ -5,6 +5,7 @@ from models import *
 from routers.users import router as users_router
 from routers.products import router as products_router
 from routers.orders import router as orders_router
+from routers.shopping_cart import router as shopping_cart_router
 
 
 app = FastAPI(title="E-Commerce app")
@@ -12,8 +13,16 @@ Base.metadata.create_all(bind=engine)
 app.include_router(users_router)
 app.include_router(products_router)
 app.include_router(orders_router)
-
+app.include_router(shopping_cart_router)
 
 @app.get("/")
 def root():
     return {"message": "Hello World!"}
+
+
+async def get_redis():
+    client = redis.from_url("redis://localhost:6379")
+    try:
+        yield client
+    finally:
+        await client.close()  # in database.py
