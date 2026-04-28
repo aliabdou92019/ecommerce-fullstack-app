6596a0854709489866e1d421ef63fd2ceda2e25e
Youssef <yosefwaheed534@gmail.com>
2026-04-28 16:58:17 +0300

    Added categories router to main

--- main_app/main.py
+++ main_app/main.py
@@ -6,13 +6,14 @@ from routers.users import router as users_router
 from routers.products import router as products_router
 from routers.orders import router as orders_router
 from routers.shopping_cart import router as shopping_cart_router
-
+from routers.categories import router as categories_router
 
 app = FastAPI(title="E-Commerce app")
 #Base.metadata.drop_all(bind=engine)
 Base.metadata.create_all(bind=engine)
 app.include_router(users_router)
 app.include_router(products_router)
+app.include_router(categories_router)
 app.include_router(orders_router)
 app.include_router(shopping_cart_router)
 
